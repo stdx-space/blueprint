052a09c9bbbb639334b0f3bb1d9dcb7aa9bd787e
Kristopher <890207+0xinterface@users.noreply.github.com>
2024-05-07 22:27:38 +0800

    Update outputs.tf

--- modules/proxmox/outputs.tf
+++ modules/proxmox/outputs.tf
@@ -1,3 +1,7 @@
 output "ip_address" {
   value = flatten(proxmox_virtual_environment_vm.this.ipv4_addresses)[1]
 }
+
+output "interface_name" {
+  value = [for name in proxmox_virtual_environment_vm.this.network_interface_names : name if startswith(name, "e")][0]
+}
