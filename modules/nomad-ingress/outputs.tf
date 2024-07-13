74be24e26ed9af4b156fd597170a0ade7def1c22
Kristopher <890207+0xinterface@users.noreply.github.com>
2024-07-13 12:45:45 +0800

    added cf tunnel id to ingress output

--- modules/nomad-ingress/outputs.tf
+++ modules/nomad-ingress/outputs.tf
@@ -2,3 +2,8 @@ output "cloudflare_tunnel_domain" {
   value       = cloudflare_tunnel.ingress.cname
   description = "The domain name of the Cloudflare tunnel pointed to traefik"
 }
+
+output "cloudflare_tunnel_id" {
+  value       = cloudflare_tunnel.ingress.id
+  description = "Tunnel ID for the created resource"
+}
