58a221830a05b0b3b581cde44d51fe3743392a99
Kristopher Lam <root@sandb0x.io>
2024-10-24 17:54:49 +0800

    added compatibility flag

--- registry/main.tf
+++ registry/main.tf
@@ -3,10 +3,12 @@ data "external" "env" {
 }
 
 resource "cloudflare_worker_script" "registry" {
-  account_id = data.external.env.result["CLOUDFLARE_ACCOUNT_ID"]
-  name       = "registry"
-  content    = file("${path.module}/dist/index.js")
-  module     = true
+  account_id          = data.external.env.result["CLOUDFLARE_ACCOUNT_ID"]
+  name                = "registry"
+  content             = file("${path.module}/dist/index.js")
+  module              = true
+  compatibility_date  = "2024-10-22"
+  compatibility_flags = ["nodejs_compat"]
 
   r2_bucket_binding {
     name        = "artifact"
