b18ca09b3f78062633c516e4373a8f14d106e31b
Kristopher Lam <root@sandb0x.io>
2024-11-04 11:50:51 +0800

    consolidate ory stack db config

--- modules/nomad-ory-hydra-kratos/outputs.tf
+++ modules/nomad-ory-hydra-kratos/outputs.tf
@@ -1,11 +1,3 @@
-output "hydra_db_password" {
-  value = var.hydra_database_password == "" ? random_password.hydra_db_password[0].result : var.hydra_database_password
-}
-
-output "kratos_db_password" {
-  value = var.kratos_database_password == "" ? random_password.kratos_db_password[0].result : var.kratos_database_password
-}
-
 output "kratos_cookie_secret" {
   value = random_bytes.kratos_cookie_secret.hex
 }
