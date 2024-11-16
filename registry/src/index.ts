7bf51bfa229d84d2fdc4af0fa085a31d5d11207c
Kristopher <root@sandb0x.io>
2024-11-16 16:13:27 +0800

    updated registry api

--- registry/src/index.ts
+++ registry/src/index.ts
@@ -6,42 +6,46 @@ import { requestId } from 'hono/request-id';
 export type Env = {
 	artifact: R2Bucket;
 };
-
-const registry = new Hono<{ Bindings: Env }>();
-registry.use(logger());
-registry.use('*', requestId());
-
 const basePath = '/v1/modules/';
+
+const app = new Hono<{ Bindings: Env }>();
+const registry = new Hono().basePath(basePath);
+app.use(logger());
+app.use('*', requestId());
+
 const serviceDiscoveryResponse = {
-	"modules.v1": basePath,
+	'modules.v1': basePath,
 };
 
-registry.get('/healthz', async (context: Context) => context.json({ status: 'ok' }));
+app.get('/healthz', async (context: Context) => context.json({ status: 'ok' }));
+app.get('/', async (context: Context) => context.json(serviceDiscoveryResponse));
+app.get('/.well-known/terraform.json', async (context: Context) => context.json(serviceDiscoveryResponse));
 
-registry.get('/', async (context: Context) =>
-	context.json(serviceDiscoveryResponse)
-);
+registry.post(`/`, async (context: Context) => {});
 
-registry.get('/.well-known/terraform.json', async (context: Context) =>
-	context.json(serviceDiscoveryResponse)
-);
+registry.get(`/`, async (context: Context) => {});
 
-registry.get(`${basePath}/:namespace/:name/:system/versions`, async (context: Context) => {
+registry.put(`/:namespace/:name/:system`, async (context: Context) => {});
+
+registry.get(`/:namespace/:name/:system/versions`, async (context: Context) => {
 	const namespace = context.req.param('namespace');
 	const name = context.req.param('name');
 	const system = context.req.param('system');
 	const result: R2Objects = await context.env.artifact.list({
-		prefix: `${namespace}/${name}/${system}`
+		prefix: `${namespace}/${name}/${system}`,
 	});
 	if (result.objects.length > 0) {
 		context.json({
-			"modules": [
+			modules: [
 				{
-					"versions": result.objects.map((object) => ({
-						version: object.key.split('/').reverse()[0].replace(/\.tgz$|\.zip|\.tar.gz$/, '')
+					versions: result.objects.map((object) => ({
+						version: object.key
+							.split('/')
+							.reverse()[0]
+							.replace(/\.tgz$|\.zip|\.tar.gz$/, ''),
 					})),
-				}
-			]
+				},
+			],
 		});
 	} else {
 		throw new HTTPException(404, {
@@ -49,29 +53,32 @@ registry.get(`${basePath}/:namespace/:name/:system/versions`, async (context: Co
 				JSON.stringify({
 					status: 'error',
 					message: 'module not found',
-				}),
+				})
 			),
 		});
 	}
 });
 
-registry.get(`${basePath}/:namespace/:name/:system/download`, async (context: Context) => {
-	context.json({
-		status: 'error',
-		message: 'not implemented'
-	}, 501)
+registry.get(`/:namespace/:name/:system/download`, async (context: Context) => {
+	context.json(
+		{
+			status: 'error',
+			message: 'not implemented',
+		},
+		501
+	);
 });
 
-registry.get(`${basePath}/:namespace/:name/:system/:version/download`, async (context: Context) => {
+registry.get(`/:namespace/:name/:system/:version/download`, async (context: Context) => {
 	const namespace = context.req.param('namespace');
 	const name = context.req.param('name');
 	const system = context.req.param('system');
 	const version = context.req.param('version');
 	const result: R2Objects = await context.env.artifact.list({
-		prefix: `${namespace}/${name}/${system}/${version}`
+		prefix: `${namespace}/${name}/${system}/${version}`,
 	});
 	if (result.objects.length > 0) {
-		context.header('X-Terraform-Get', `https://artifact.narwhl.dev/${result.objects[0].key}`);
+		context.header('X-Terraform-Get', `https://artifact.narwhl.dev/modules/${result.objects[0].key}`);
 		context.status(204);
 	} else {
 		throw new HTTPException(404, {
@@ -79,10 +86,11 @@ registry.get(`${basePath}/:namespace/:name/:system/:version/download`, async (co
 				JSON.stringify({
 					status: 'error',
 					message: 'module not found',
-				}),
+				})
 			),
 		});
 	}
 });
 
-export default registry;
+app.route('/', registry);
+export default app;
