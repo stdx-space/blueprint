2cd1fff4bdd191e263330ca4dc64fea074e67896
Tommy Li <dev@stdx.space>
2024-11-19 17:36:43 +0800

    feat: create typesense module

---
+++ modules/nomad-typesense/README.md
@@ -0,0 +1,38 @@
+# Terraform module for self hosting Typesense on Nomad
+
+## Usage
+
+```hcl
+module "typesense" {
+  source                = "github.com/narwhl/blueprint//modules/nomad-typesense"
+  datacenter_name       = local.datacenter_name      # Nomad datacenter name
+  typesense_version     = "26.0"                     # Typesense version
+  typesense_api_key     = "<your-typesense-api-key>" # Typesense API key
+  enable_ephemeral_disk = true                       # Enable Nomad ephemeral disk for the storing typesense data temporarily. Cannot be used with host volumes.
+  purge_on_destroy      = true                       # Purge Typesense job on destroy
+}
+```
+
+### Host volumes
+
+One may use host volumes for persisting typesense data.
+
+```hcl
+module "typesense" {
+...
+  host_volume_config = {
+    source = "host-volume-name"
+    read_only = false
+  }
+}
+```
+
+Remember to update `/etc/nomad.d/nomad.hcl` configuration to create the host
+volume. This should be under the `client` stanza.
+
+```hcl
+host_volume "host-volume-name" {
+  path      = "/opt/typesense/data"
+  read_only = false
+}
+```
---
+++ modules/nomad-typesense/variables.tf
@@ -0,0 +1,46 @@
+variable "datacenter_name" {
+  type        = string
+  description = "Name of the datacenter"
+  validation {
+    condition     = length(var.datacenter_name) > 0
+    error_message = "Datacenter name cannot be empty"
+  }
+}
+
+variable "job_name" {
+  default     = "typesense"
+  description = "Nomad job name"
+}
+
+variable "typesense_version" {
+  default     = "latest"
+  description = "Typesense version to be deployed"
+}
+
+variable "typesense_api_key" {
+  default     = ""
+  sensitive   = true
+  description = "Typesense API key. Leave blank to generate a new one with Terraform random resource."
+}
+
+variable "host_volume_config" {
+  type = object({
+    source    = string
+    read_only = optional(bool, false)
+  })
+  nullable    = true
+  default     = null
+  description = "Host volume configuration for storing typesense data"
+}
+
+variable "enable_ephemeral_disk" {
+  type        = bool
+  default     = false
+  description = "Enable Nomad ephemeral disk for the storing typesense data temporarily. Cannot be used with host volumes."
+}
+
+variable "purge_on_destroy" {
+  type        = bool
+  default     = false
+  description = "Purge the Typesense Nomad job on destroy"
+}
