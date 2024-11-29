d220d546c3a9f8c66fcfdc8397ff181cba3a648e
Tommy Li <dev@stdx.space>
2024-11-29 10:51:04 +0800

    feat: backport spilo module options to postgres

--- modules/nomad-postgres/outputs.tf
+++ modules/nomad-postgres/outputs.tf
@@ -1,8 +1,8 @@
 output "postgres_passwords" {
   value = merge({
-    superuser = random_password.postgres_superuser_password.result
+    superuser = local.postgres_superuser_password
     }, {
-    for db in local.postgres_init_result : db.user => db.password
+    for db in local.postgres_init_result : db.user => db.password if db.create_user
   })
   sensitive   = true
   description = "The PostgreSQL passwords generated"
