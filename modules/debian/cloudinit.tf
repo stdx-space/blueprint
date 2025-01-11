5e507a885dc09d1c3ad9c6a8249ff58bd44e88d3
Kristopher <root@sandb0x.io>
2025-01-11 11:54:46 +0800

    set auto reboot for debian cloudinit by default

--- modules/debian/cloudinit.tf
+++ modules/debian/cloudinit.tf
@@ -8,84 +8,80 @@ data "cloudinit_config" "user_data" {
     content = join("\n", [
       "#cloud-config",
       yamlencode(
-        merge(
-          {
-            system_info = {
-              apt_get_command = [
-                "apt-get",
-                "--option=Dpkg::Options::=--force-confold",
-                "--option=Dpkg::options::=--force-unsafe-io",
-                "--assume-yes",
-                "--quiet",
-                "--no-install-recommends",
-              ]
-            }
-            apt = {
-              preserve_sources_list = true
-              sources               = local.repositories
-            }
-            users = local.users
-            bootcmd = [
-              "echo 'blacklist rfkill\nblacklist cfg80211' | tee -a /etc/modprobe.d/blacklist.conf",
-              "apt-get update && apt-get install -y gnupg ${contains(flatten(var.substrates.*.install.repositories), "nvidia-container-toolkit") ? "linux-headers-$(uname -r)" : ""}"
+        {
+          system_info = {
+            apt_get_command = [
+              "apt-get",
+              "--option=Dpkg::Options::=--force-confold",
+              "--option=Dpkg::options::=--force-unsafe-io",
+              "--assume-yes",
+              "--quiet",
+              "--no-install-recommends",
             ]
-            cloud_init_modules = concat(
-              [
-                "bootcmd",
-                "write_files",
-                "set_hostname",
-                "ssh", # regenerate ssh host keys
-                "users-groups",
-                "growpart", # expand root partition to fill disk
-                "resizefs", # resize filesystem
-              ],
-              0 < length(var.disks) ? ["disk_setup", "fs_setup", "mounts"] : [],
-              0 < length(var.ca_certs) ? ["ca_certs"] : []
-            )
-            cloud_config_modules = [
-              "apt_configure",
-              "timezone",
-              "update_etc_hosts",
-              "runcmd"
+          }
+          apt = {
+            preserve_sources_list = true
+            sources               = local.repositories
+          }
+          users = local.users
+          bootcmd = [
+            "echo 'blacklist rfkill\nblacklist cfg80211' | tee -a /etc/modprobe.d/blacklist.conf",
+            "apt-get update && apt-get install -y gnupg ${contains(flatten(var.substrates.*.install.repositories), "nvidia-container-toolkit") ? "linux-headers-$(uname -r)" : ""}"
+          ]
+          cloud_init_modules = concat(
+            [
+              "bootcmd",
+              "write_files",
+              "set_hostname",
+              "ssh", # regenerate ssh host keys
+              "users-groups",
+              "growpart", # expand root partition to fill disk
+              "resizefs", # resize filesystem
+            ],
+            0 < length(var.disks) ? ["disk_setup", "fs_setup", "mounts"] : [],
+            0 < length(var.ca_certs) ? ["ca_certs"] : []
+          )
+          cloud_config_modules = [
+            "apt_configure",
+            "timezone",
+            "update_etc_hosts",
+            "runcmd"
+          ]
+          cloud_final_modules = [
+            "package_update_upgrade_install",
+            "scripts_user", # required for runcmd
+            "power_state_change",
+            "write-files-deferred",
+          ]
+          hostname         = var.name
+          manage_etc_hosts = true
+          timezone         = var.timezone
+          packages         = local.packages
+          ca_certs         = local.ca_certs
+          disk_setup       = local.disks
+          fs_setup         = local.filesystems
+          write_files      = local.files
+          mounts = [
+            for disk in var.disks : [
+              disk.device_path,
+              disk.mount_path,
             ]
-            cloud_final_modules = [
-              "package_update_upgrade_install",
-              "scripts_user", # required for runcmd
-              "power_state_change",
-              "write-files-deferred",
-            ]
-            hostname         = var.name
-            manage_etc_hosts = true
-            timezone         = var.timezone
-            packages         = local.packages
-            ca_certs         = local.ca_certs
-            disk_setup       = local.disks
-            fs_setup         = local.filesystems
-            write_files      = local.files
-            mounts = [
-              for disk in var.disks : [
-                disk.device_path,
-                disk.mount_path,
-              ]
-            ]
-            runcmd = concat(
-              [for dir in local.directories : "mkdir -m ${dir.mode} -p ${dir.path}"],
-              [for dir in local.directories : "chown -R ${dir.owner}:${dir.group} ${dir.path}"],
-              var.startup_script.override_default ? [] : [
-                "systemctl daemon-reload",
-                "systemctl enable qemu-guest-agent --now",
-                "systemctl restart --no-block systemd-resolved systemd-networkd",
-              ],
-              var.startup_script.inline
-            )
-          },
-          contains(flatten(var.substrates.*.install.repositories), "nvidia-container-toolkit") ? {
-            power_state = {
-              delay = "now"
-              mode  = "reboot"
-            }
-          } : {}
-        )
+          ]
+          runcmd = concat(
+            [for dir in local.directories : "mkdir -m ${dir.mode} -p ${dir.path}"],
+            [for dir in local.directories : "chown -R ${dir.owner}:${dir.group} ${dir.path}"],
+            var.startup_script.override_default ? [] : [
+              "systemctl daemon-reload",
+              "systemctl enable qemu-guest-agent --now",
+              "systemctl restart --no-block systemd-resolved systemd-networkd",
+            ],
+            var.startup_script.inline
+          )
+          power_state = {
+            delay = "now"
+            mode  = "reboot"
+          }
+        }
       )
     ])
   }
