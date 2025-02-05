644be0bdb67a6c48d3d92d024cda612e67355b68
Kristopher Lam <root@sandb0x.io>
2025-02-05 19:08:27 +0800

    fixes tailscale source

--- modules/tailscale/main.tf
+++ modules/tailscale/main.tf
@@ -1,3 +1,7 @@
+data "http" "upstream" {
+  url = var.supplychain
+}
+
 locals {
   systemd_units = [
     {
--- modules/tailscale/variables.tf
+++ modules/tailscale/variables.tf
@@ -1,3 +1,8 @@
+variable "supplychain" {
+  type    = string
+  default = "https://artifact.narwhl.dev/upstream/current.json"
+}
+
 variable "auth_key" {
   type        = string
   description = "Tailscale auth key"
