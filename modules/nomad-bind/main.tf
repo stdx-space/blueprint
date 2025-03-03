712330808e980337a313c3c737589f7b094e0595
Kristopher Lam <root@sandb0x.io>
2025-03-03 22:49:49 +0800

    fixes template placement

--- modules/nomad-bind/main.tf
+++ modules/nomad-bind/main.tf
@@ -11,13 +11,14 @@ resource "nomad_variable" "secrets" {
 }
 
 resource "nomad_job" "bind" {
-  jobspec = templatefile("${path.module}/bind.nomad.tftpl", {
+  jobspec = templatefile("${path.module}/templates/bind.nomad.hcl.tftpl", {
+    job_name             = var.job_name
+    datacenter_name      = var.datacenter_name
+    namespace            = var.namespace
     bind_version         = var.bind_version
     tailscale_version    = var.tailscale_version
-    tailscale_authkey    = var.tailscale_authkey
     zones                = var.zones
     upstream_nameservers = var.upstream_nameservers
-    secret               = random_bytes.secret.base64
     resources            = var.resources
   })
   purge_on_destroy = var.purge_on_destroy
