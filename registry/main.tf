ec01f40503532b4df514e1e3cc79f83709c7fa50
Kristopher Lam <root@sandb0x.io>
2025-05-06 18:02:24 +0800

    upgrades cf tf provider

--- registry/main.tf
+++ registry/main.tf
@@ -7,24 +7,24 @@ resource "cloudflare_workers_kv_namespace" "registry" {
   title      = "modules"
 }
 
-# resource "cloudflare_workers_script" "registry" {
-#   account_id          = data.external.env.result["CLOUDFLARE_ACCOUNT_ID"]
-#   script_name         = "registry"
-#   content             = file("${path.module}/dist/index.js")
-#   main_module         = "index.js"
-#   compatibility_date  = "2024-11-06"
-#   compatibility_flags = ["nodejs_compat"]
+resource "cloudflare_workers_script" "registry" {
+  account_id          = data.external.env.result["CLOUDFLARE_ACCOUNT_ID"]
+  script_name         = "registry"
+  content             = file("${path.module}/dist/index.js")
+  main_module         = "index.js"
+  compatibility_date  = "2024-11-06"
+  compatibility_flags = ["nodejs_compat"]
 
-#   bindings = [
-#     {
-#       type         = "kv_namespace"
-#       namespace_id = cloudflare_workers_kv_namespace.registry.id
-#       name         = "modules"
-#     },
-#     {
-#       type        = "r2_bucket"
-#       name        = "artifact"
-#       bucket_name = "artifact"
-#     }
-#   ]
-# }
+  bindings = [
+    {
+      type         = "kv_namespace"
+      namespace_id = cloudflare_workers_kv_namespace.registry.id
+      name         = "modules"
+    },
+    {
+      type        = "r2_bucket"
+      name        = "artifact"
+      bucket_name = "artifact"
+    }
+  ]
+}
