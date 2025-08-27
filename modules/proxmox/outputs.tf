60a7e5044992f9cd72322c0386cb0d80b11b2237
Kristopher Lam <root@sandb0x.io>
2025-08-27 11:44:35 +0800

    patched proxmox ip_address output

--- modules/proxmox/outputs.tf
+++ modules/proxmox/outputs.tf
@@ -1,5 +1,11 @@
 output "ip_address" {
-  value = flatten(proxmox_virtual_environment_vm.this.ipv4_addresses)[1]
+  value = tolist([
+    for ip in flatten(proxmox_virtual_environment_vm.this.ipv4_addresses) :
+    ip if !(
+      startswith(ip, "169.254") ||
+      startswith(ip, "127.0.0")
+    )
+  ])[0]
 }
 
 output "interface_name" {
