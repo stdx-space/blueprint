c72d8ae1ccd054893b01293301cf934ca2089193
Kristopher Lam <root@sandb0x.io>
2025-09-16 13:39:16 +0800

    fix(proxmox): add digest resource for provisioning config and update lifecycle triggers

--- modules/proxmox/proxmox.tf
+++ modules/proxmox/proxmox.tf
@@ -17,6 +17,10 @@ resource "proxmox_virtual_environment_file" "provisioning_config" {
   }
 }
 
+resource "terraform_data" "digest" {
+  input = md5(var.provisioning_config.payload)
+}
+
 resource "proxmox_virtual_environment_vm" "this" {
   name      = var.name
   node_name = var.node
@@ -107,5 +111,9 @@ resource "proxmox_virtual_environment_vm" "this" {
       interface         = local.cloudinit_drive_interface[var.firmware]
       user_data_file_id = proxmox_virtual_environment_file.provisioning_config[initialization.key].id
     }
+  }
+
+  lifecycle {
+    replace_triggered_by = [terraform_data.digest]
   }
 }
